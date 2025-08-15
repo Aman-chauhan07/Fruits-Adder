b5a3ef75eba628a1bd13f6e1ff2c7869aacbb0c2
Aman-chauhan07 <ameye0715@gmail.com>
2025-08-15 22:10:51 +0530

    add fruits

---
+++ src/Lists.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./index.css"
+
+function Lists() {
+  const [fruits, setFruits] = useState(["Apple", "Mango", "Orange"]);
+
+
+  //Add food
+  function addFruit() {
+
+   const newfoods = document.getElementById("foodInput").value.trim()
+    if (newfoods === "") {
+      return; // empty ya sirf spaces ho to kuch add mat karo
+    }
+   document.getElementById("foodInput").value = ""
+   setFruits(f => [...f,newfoods])
+  }
+// remove the item
+ function removefood(index){
+     setFruits(fruits.filter((_, i) => i !== index));
+ }
+  return (
+    <div className="container">
+      <h2>List of Fruits 🍓</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index} 
+          onDoubleClick={()=>removefood(index)
+            }
+           style={{ cursor: "pointer" }}
+           >
+            {fruit}
+            </li>
+        ))}
+      </ul>
+      <input type="text" placeholder="Add Fruits" id="foodInput"/>
+      <button onClick={addFruit}>Add Fruits</button>
+    </div>
+  );
+}
+
+export default Lists;
